7cfa9f795e65278dfbc9f1aae01c1ac6c6040b57
jbus97 <hugofleitasbus@gmail.com>
2021-11-16 10:56:29 -0300

    Agrega Venta

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { VentaComponent } from './venta/venta.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "ventas",
+    component: VentaComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
