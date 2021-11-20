615774fc002a6e0e234a2544c8f01b967a5a7b2e
jbus97 <hugofleitasbus@gmail.com>
2021-11-20 13:21:33 -0300

    Pantalla de registro de ventas

--- src/app/venta/venta.component.ts
+++ src/app/venta/venta.component.ts
@@ -13,9 +13,8 @@ export class VentaComponent implements OnInit {
   venta: CabeceraVenta = new CabeceraVenta();
   productos: Producto[] = [];
   constructor(private productoService: ProductoService) { }
-
   ngOnInit(): void {
-    this.productoService.delete("1").then();
+    this.venta.total = 0;
     this.productoService.getAll().then(data => {
       console.log(data);
       data.map(producto => this.productos.push({
@@ -29,17 +28,21 @@ export class VentaComponent implements OnInit {
   }
 
   agregarDetalle(){
-    if (this.venta.detalles){
-      let detalle = new DetalleVenta()
-      this.venta.detalles.push(detalle)
-    }else{
+    if (!this.venta.detalles){
       this.venta.detalles = []
-      let detalle = new DetalleVenta()
-      this.venta.detalles.push(detalle)
     }
+      
+      let detalle = new DetalleVenta()
+      detalle.totalDetalle = 0
+      this.venta.detalles.push(detalle)
+  
     
     console.log(this.venta)
   }
 
-  
+  actualizarTotalDetalle(detalle: DetalleVenta){
+    if (detalle.producto && detalle.cantidad){
+      detalle.totalDetalle = detalle.producto.precioVenta * detalle.cantidad
+    }
+  }
 }
