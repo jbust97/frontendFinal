e7b724b18423659e6fe929f08ffff82e16840a8d
RaulGalvan <ramako72@gmail.com>
2021-11-21 20:16:11 -0300

    ultimos cambios

--- src/app/cliente/modificar-cliente/modificar-cliente.component.ts
+++ src/app/cliente/modificar-cliente/modificar-cliente.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Cliente } from 'src/app/models/cliente';
+import { ClienteService } from 'src/app/service/cliente.service';
 
 @Component({
   selector: 'app-modificar-cliente',
@@ -7,10 +10,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ModificarClienteComponent implements OnInit {
 
-  constructor() { }
+  cliente: Cliente = new Cliente();
+
+  constructor(private clienteService: ClienteService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-    
+    this.route.paramMap.subscribe(paramMap => {
+      this.cliente.id = paramMap.get('id') ?? '';
+      this.clienteService.retrieve(this.cliente.id)
+        .then(data => {
+          this.cliente.id = (data as any).id;
+          this.cliente.nombreCompleto = (data as any).nombreCompleto;
+          this.cliente.ruc = (data as any).ruc;
+          this.cliente.email = (data as any).email;
+        });
+    });
+  }
+
+  modificarCliente() {
+    this.clienteService.update(this.cliente).then(() => window.location.pathname = "cliente");
   }
 
 }
