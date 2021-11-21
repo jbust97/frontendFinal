51ff773557f377bf7e4a184f75ddbd15cda711d7
Ricardo Gaona <gaonaricardo06@gmail.com>
2021-11-21 19:05:00 -0300

    Se implementa la modificacion de productos

--- src/app/producto/modificar-producto/modificar-producto.component.ts
+++ src/app/producto/modificar-producto/modificar-producto.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Producto } from 'src/app/models/producto';
+import { ProductoService } from 'src/app/service/producto.service';
+import { DocumentData } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-modificar-producto',
@@ -6,10 +10,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./modificar-producto.component.css']
 })
 export class ModificarProductoComponent implements OnInit {
+  producto: Producto = new Producto();
 
-  constructor() { }
+  constructor(private productoService: ProductoService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe(paramMap => {
+      this.producto.id = paramMap.get('id') ?? '';
+      this.productoService.retrieve(this.producto.id)
+        .then(data => {
+          this.producto.codigo = (data as any).codigo;
+          this.producto.nombre = (data as any).nombre;
+          this.producto.precioVenta = (data as any).precioVenta;
+          this.producto.existencia = (data as any).existencia;
+        });
+    });
+  }
+
+  modificarProducto() {
+    this.productoService.update(this.producto).then(() => window.location.pathname = "productos");
   }
 
 }
